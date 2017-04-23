93edad6245c1c05ef72d7c76268d06da3e9e2c11
Abigail Watson <awatson1978@gmail.com>
2017-04-23 02:48:13 -0500

    updates

--- server/meteor.methods.js
+++ server/meteor.methods.js
@@ -1,17 +1,17 @@
 
 
 Meteor.methods({
-  createPractitioner:function(practitionerObject){
-    check(practitionerObject, Object);
+  createPractitionerRole:function(practitionerRoleObject){
+    check(practitionerRoleObject, Object);
 
     if (process.env.NODE_ENV === 'test') {
-      console.log('Creating Practitioner...');
-      Practitioners.insert(practitionerObject, function(error, result){
+      console.log('Creating PractitionerRole...');
+      PractitionerRoles.insert(practitionerRoleObject, function(error, result){
         if (error) {
           console.log(error);
         }
         if (result) {
-          console.log('Practitioner created: ' + result);
+          console.log('PractitionerRole created: ' + result);
         }
       });
     } else {
@@ -19,35 +19,35 @@ Meteor.methods({
       console.log('Try setting NODE_ENV=test');
     }
   },
-  initializePractitioner:function(){
-    if (Practitioners.find().count() === 0) {
-      console.log("No records found in Practitioners collection.  Lets create some...");
+  // initializePractitionerRole:function(){
+  //   if (PractitionerRoles.find().count() === 0) {
+  //     console.log("No records found in PractitionerRoles collection.  Lets create some...");
 
-      var defaultPractitioner = {
-        name: {
-          given: ["Gregory"],
-          family: ["House"],
-          text: "Dr. Gregory House, M.D."
-        },
-        telecom: [{
-          system: 'phone',
-          value: '212-555-2345',
-          use: 'work',
-          rank: '1'
-        }]
-      };
+  //     var defaultPractitionerRole = {
+  //       name: {
+  //         given: ["Gregory"],
+  //         family: ["House"],
+  //         text: "Dr. Gregory House, M.D."
+  //       },
+  //       telecom: [{
+  //         system: 'phone',
+  //         value: '212-555-2345',
+  //         use: 'work',
+  //         rank: '1'
+  //       }]
+  //     };
 
-      Meteor.call('createPractitioner', defaultPractitioner);
-    } else {
-      console.log('Practitioners already exist.  Skipping.');
-    }
-  },
-  dropPractitioners: function(){
+  //     Meteor.call('createPractitionerRole', defaultPractitionerRole);
+  //   } else {
+  //     console.log('PractitionerRoles already exist.  Skipping.');
+  //   }
+  // },
+  dropPractitionerRoles: function(){
     if (process.env.NODE_ENV === 'test') {
       console.log('-----------------------------------------');
-      console.log('Dropping practitioners... ');
-      Practitioners.find().forEach(function(practitioner){
-        Practitioners.remove({_id: practitioner._id});
+      console.log('Dropping practitionerRoles... ');
+      PractitionerRoles.find().forEach(function(practitionerRole){
+        PractitionerRoles.remove({_id: practitionerRole._id});
       });
     } else {
       console.log('This command can only be run in a test environment.');
